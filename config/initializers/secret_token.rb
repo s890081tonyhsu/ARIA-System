7e0061c86ab70ca40a933f7618011c153006a6ce
OtomeSound <s890081tonyhsu@gmail.com>
2014-04-23 04:44:15 +0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ARIASystem::Application.config.secret_key_base = 'a945ad336355ee98782d6cdb531c8bbb984c9e0f30668f8b6df45273aa558ec738f25f9894bcfd07c9414f46c06bb4e4e7dd839d6167935aa3a7f55680fbb90d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ARIASystem::Application.config.session_store :cookie_store, key: '_ARIA-System_session'
